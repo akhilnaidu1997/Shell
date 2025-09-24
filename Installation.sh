8f25e758614e11910de6ee0acf91573f486f2d3b
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-24 12:14:45 +0530

    message

--- Installation.sh
+++ Installation.sh
@@ -6,6 +6,11 @@ Y="\e[33m"
 N="\e[0m"
 
 USER=$(id -u) 
+LOG_FOLDER1="/var/log/shell-install"
+SCRIPT_NAME1=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER1/$SCRIPT_NAME1.log"
+
+mkdir -p $LOG_FOLDER1
 
 if [ $USER -ne 0 ]; then
     echo -e "$R ERROR:: Install with sudo permissions $N"
@@ -20,25 +25,25 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf install mysql -y &>> $LOG_FILE
     VALIDATE $? "MYSQL"
 else
     echo -e "$Y MYSQL is already installed $N"
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>> $LOG_FILE
     VALIDATE $? "NGINX"
 else
     echo -e "$Y NGINX is already installed $N"
 fi
 
-dnf list installed python3
+dnf list installed python3 &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y &>> $LOG_FILE
     VALIDATE $? "PYTHON"
 else
     echo -e "$Y PYTHON# is already installed $N"
