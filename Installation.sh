f890efb994f7d0683cf9faee6165434128378251
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-24 11:09:48 +0530

    message

--- Installation.sh
+++ Installation.sh
@@ -4,12 +4,14 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]; then
     echo "ERROR:: Install with sudo permissions"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Installing mysql is failure"
+    exit 1
 else 
     echo "MYSQL app is already installed"
 fi
