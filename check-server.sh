60efe4ccbc18b63dcdd6847172578261bbb6f0bb
Akhil Naidu <akhilnaid1997@gmail.com>
2026-05-26 11:30:42 +0530

    check-servers

--- check-server.sh
+++ check-server.sh
@@ -5,5 +5,7 @@ SERVERS=("34.228.17.213")
 for server in "${SERVERS[@]}"
 do
     echo "Checking server: $server"
-    ssh ec2-user@"$server" "df -hT"
+    ssh -o ConnectTimeout=5 ec2-user@"$server" "df -hT"
+    ssh -o ConnectTimeout=5 ec2-user@"$server" "free -h"
+    # ssh -o ConnectTimeout=5 ec2-user@"$server" "top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2 "%"}' "  
 done
